45909ba3530402656ab24b9b01a4f50548fe96d8
Bana <b.teklay@live.co.uk>
2025-09-25 19:58:22 +0100

    Update index.js

--- itol-js-react/simple-banking-system/index.js
+++ itol-js-react/simple-banking-system/index.js
@@ -1,1 +1,13 @@
+import { bankAccount } from "./bankAccount.js";
 
+const client1 = new bankAccount(56433, "Daniel", 400);
+client1.deposit(250);
+console.log(client1.checkBalance());
+
+const client2 = new bankAccount(47876, "Cindy", 2500);
+client2.withdraw(300);
+console.log(client2.checkBalance());
+
+const client3 = new bankAccount(34580, "Janet", 25);
+client3.withdraw(50);
+console.log(client3.checkBalance());
